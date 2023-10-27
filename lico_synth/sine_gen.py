f2f19961c90a92aaae0bfed8e6b2011047b8cab3
Sabar Dasgupta <s@bardasgupta.com>
2023-10-27 15:48:30 -0400

    working synth with unison and detune!

--- lico_synth/sine_gen.py
+++ lico_synth/sine_gen.py
@@ -1,17 +1,26 @@
-if midi_out[0] != 0:
-    key = midi_out[1] - 20
-    freq = 440. * np.power(2, (key-49)/12.)
-    if midi_out[0] == 144:
-        keys_pressed[key] = freq
-    elif midi_out[0] == 128:
-        del keys_pressed[key]
+if len(midi_out) > 0:
+    print(midi_out, flush=True)
+for midi_event in midi_out:
+    if midi_event[0] == 176:
+        if midi_event[1] == 7: # volume
+            amplitude = 1000. * midi_event[2] / 127.
+        elif midi_event[1] == 1: # modulation
+            pass # TODO implement LFO
+    elif midi_event[0] != 0:
+        key = midi_event[1] - 8
+        freq = 440. * np.power(2, (key-49)/12.)
+        if midi_event[0] == 144:
+            keys_pressed[key] = freq
+        elif midi_event[0] == 128:
+            if key in keys_pressed.keys():
+                del keys_pressed[key]
 
 if len(keys_pressed) == 0:
     res = zeros
 else:
     freqs = keys_pressed.values()
     res = np.sum(
-        [np.sin(2 * np.pi * freq * counter_interleaved) for freq in freqs], axis=0
+        [synth.wave(2 * np.pi * freq * counter_interleaved) for freq in freqs], axis=0
     )[:]
 
 if not stereo_out[0]:
@@ -24,5 +33,5 @@ if not stereo_out[1]:
         if i % 2 == 1:  # 1 for right
             res[i] = 0
 
-sine_wave[:] = (res * 1000).astype(np.int16)
+sine_wave[:] = (res * amplitude).astype(np.int16)
 counter_interleaved += timestep
--- lico_synth/sine_gen_constructor.py
+++ lico_synth/sine_gen_constructor.py
@@ -1,3 +1,6 @@
+import scipy
+import numpy as np
+
 # sampling_rate = 48000  # TODO make this available from config
 sampling_rate = {{config["config"]["sampling_rate"]}}
 timestep = 0.01
@@ -11,3 +14,38 @@ zeros = np.zeros(960)
 stereo_out = (True, False)  # left/right
 
 keys_pressed = {}
+
+amplitude = 1000
+
+
+def square_plus_saw(t):
+    return scipy.signal.square(t) + scipy.signal.sawtooth(t)
+
+
+# signal_func = np.sin
+signal_func = scipy.signal.sawtooth
+# signal_func = scipy.signal.square
+# signal_func = square_plus_saw
+
+
+class Synth():
+
+    def __init__(self, wave, unison=None, detune=None):
+        if unison or detune:
+            self.set_unison_detune(wave, unison, detune)
+        else:
+            self.wave = wave
+
+    def set_unison_detune(self, wave, unison, detune):
+        detune_freqs = np.linspace(1. - detune, 1. + detune, unison)
+
+        def new_wave(t):
+            waves = []
+            for i in range(unison):
+                waves.append(wave(t * detune_freqs[i]))
+            return np.sum(np.array(waves), axis=0)
+
+        self.wave = new_wave
+
+
+synth = Synth(signal_func, 13, 0.6)
